c15ed3489204fd1cc04c142af811328a3acd24b2
gunheelee-c106-20 <qsd0812@me.com>
2019-10-21 18:58:11 +0900

    Drill 8-2 완성
    2015182029 이건희
    https://youtu.be/31CJbsBX3SQ
    커밋횟수 : 1회

--- Drill-8/pause_state.py
+++ Drill-8/pause_state.py
@@ -4,10 +4,12 @@ import main_state
 import title_state
 name = 'PauseState'
 image = None
-logo_time = 0
+logo_time =0
 def enter():
     global image
+    global logo_time
     image = load_image('pause.png')
+    logo_time=0
     pass
 
 
@@ -31,11 +33,15 @@ def draw():
     global image
     global logo_time
     clear_canvas()
-    image.clip_draw(0, 0, 900, 900, 400, 300, 100, 100)
-
-
-
-
+    main_state.boy.draw()
+    main_state.grass.draw()
+    if logo_time<=0.5:
+        image.clip_draw(0, 0, 900, 900, 400, 300, 100, 100)
+    elif logo_time<=1.0:
+        pass
+    else:
+        logo_time=0
+    logo_time+=0.01
     update_canvas()
     delay(0.01)
     pass
